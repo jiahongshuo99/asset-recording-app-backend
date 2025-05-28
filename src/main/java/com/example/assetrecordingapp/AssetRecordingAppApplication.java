60b6a47b339620e634d2bfcbd2d4c71eaad93a98
jiahongshuo <jiahongshuo99@gmail.com>
2025-05-28 23:25:50 +0800

    init project

---
+++ src/main/java/com/example/assetrecordingapp/AssetRecordingAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.assetrecordingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetRecordingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AssetRecordingAppApplication.class, args);
+    }
+}
